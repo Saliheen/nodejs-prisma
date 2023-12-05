41f3e455957cb595d253c5532378a9f5069602fe
SaliheenAfridi <saliheenafridi0@gmail.com>
2023-12-05 15:23:53 +0500

    db seeding

--- script.ts
+++ script.ts
@@ -1,33 +1,42 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ log: ['query'] });
+const prisma = new PrismaClient();
+const { faker } = require('@faker-js/faker');
+
+const createRandomUser = (): any => {
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    age: faker.number.int({ min: 10, max: 100 }),
+    isAdmin: false,
+  };
+};
 
 async function main() {
-  await prisma.userPreference.deleteMany();
-  await prisma.user.deleteMany();
-  const user = await prisma.user.create({
-    data: {
-      name: 'Saliheen Afridi',
-      email: 'test@gmail.com',
-      age: 25,
-      isAdmin: false,
-      userPreference: {
-        create: {
-          emailUpdates: true,
-        },
-      },
-    },
-    select: {
-      name: true,
-      userPreference: {
-        select: {
-          id: true,
-        },
-      },
-    },
-  });
+  let users = [];
+  for (let i = 0; i < 100; i++) {
+    const userData = createRandomUser();
+    users.push(userData);
+  }
 
-  console.log(user);
+  const data = await Promise.all(
+    users.map(async (u) => {
+      return prisma.user.create({ data: { ...u } });
+    })
+  );
+  console.log(data);
+  // const users = await prisma.user.findMany({
+  //   where: {
+  //     name: {
+  //       startsWith: 's',
+  //     },
+  //   },
+  //   select: {
+  //     name: true,
+  //     email: true,
+  //   },
+  // });
+  // console.log(users);
 }
 
 main()
